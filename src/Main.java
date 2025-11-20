bd6a0b5a1da4376cfd933d9b90be5070570f5108
Meng <mengsaoly@gmail.com>
2025-11-20 22:47:59 +0700

    array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("How many student:");
+        int n = in.nextInt();
+        int total = 0;
+        int[][] scor =new int[n][5];
+        String[] name = new String[n];
+
+        for(int i = 0; i<n;i++ ){
+            System.out.print("Enter name of student:");
+            name[i] = in.next();
+            for(int j = 0; j<5;j++){
+                switch (j){
+                    case 0:{
+                            System.out.print("Enter MIS:");
+                            scor[i][j]=in.nextInt();
+                        }break;
+                    case 1:{
+                        System.out.print("Enter SA:");
+                        scor[i][j]=in.nextInt();
+                    }break;
+                    case 2:{
+                        System.out.print("Enter SM:");
+                        scor[i][j]=in.nextInt();
+                    }break;
+                    case 3:{
+                        System.out.print("Enter Java:");
+                        scor[i][j]=in.nextInt();
+                    }break;
+                    case 4:{
+                        System.out.print("Enter WED2:");
+                        scor[i][j]=in.nextInt();
+                    }break;
+
+                }
+                total += scor[i][j];
+
+            }
+        }
+        System.out.println("======================================================");
+        System.out.println("List Student"+n);
+        System.out.println("ID"+"\t"+"Name"+" "+"MIS"+"\t"+"SA"+"\t"+"SM"+"\t"+"JV"+"\t"+"WD"+"\t"+"Total"+"\t");
+        for(int i = 0; i<n;i++ ){
+            System.out.print((i+1)+"\t" +name[i]+"\t" );
+            for(int j = 0; j<5;j++){
+                System.out.print(scor[i][j]+"\t");
+
+            }
+            System.out.print(total);
+            System.out.println();
         }
     }
 }
