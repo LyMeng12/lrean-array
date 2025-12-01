8b3ef7b8e9513f23cea4db9ddd283abcee545cba
Ly Meng <mengsaoly@gmail.com>
2025-12-01 11:32:32 +0700

    5%

--- src/array1.java
+++ src/array1.java
@@ -2,21 +2,74 @@ import java.util.Scanner;
 
 public class array1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=1;
-        int choose;
-        int[] a =new int[n];
-       for (int i = 0 ; i < a.length ; i++) {
-           System.out.println("1.Add");
-           System.out.print("Please choose");
-           choose = sc.nextInt();
-           switch (choose) {
-               case 1:{
+        Scanner in = new Scanner(System.in);
+//        int[] a =new int[0];
+//
+//        // add new value 4
+//
+//
+//        int[] newArray = new int[a.length + 1];
+//
+//        for(int i = 0; i < a.length; i++) {
+//            newArray[i] = a[i];
+//        }
+//        int newValue = 4;
+//
+//        newArray[a.length] = newValue;
+//
+//        // print
+//        for(int x : newArray) {
+//            System.out.print(x + " ");
+//        }
+        int m=0;
 
-               }break;
+        while (true){
+            int[] a=new int[m];
+
+
+
+           if(a.length==0) {
+               int[] b=new int[a.length+1];
+               System.out.println("1.Add");
+               System.out.print("Please Choose:");
+               int n = in.nextInt();
+               switch (n) {
+                   case 1: {
+                       System.out.print("Enter First Number:");
+                       int c = in.nextInt();
+                       b[a.length] = c;
+                       m=a.length+1;
+                   }
+                   break;
+               }
+
+           } else if (a.length>0) {
+               System.out.println("1.Add");
+               System.out.println("2.Insert");
+               System.out.println("3.Delete");
+               System.out.println("4.Update");
+               System.out.println("5.Display");
+               System.out.println("6.Sort");
+               System.out.println("7.Search");
+               System.out.print("Please Choose:");
+               int n = in.nextInt();
+                   switch (n) {
+                       case 1: {
+                           int[] b=new int[a.length+1];
+                           System.out.print("Enter First Number:");
+                           int c = in.nextInt();
+                           b[a.length] = c;
+                           m = a.length + 1;
+                       }
+                       break;
+                   }
            }
 
 
-       }
+        }
+
+
+
+
     }
 }
